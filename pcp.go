2db3ecbf0b3694e08ca0ce8c36d19bdbb8df8fff
Gourav <gouravrattihalli@outlook.com>
2016-09-16 20:03:36 +0000

    initial pcp logging script

---
+++ pcp.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	cmd := exec.Command("sh", "-c", "pmdumptext -m -l -o -d , -c config")
+	stdout, err := os.Create("./output.txt")
+	cmd.Stdout = stdout
+    fmt.Println("PCP started: ", stdout)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+	if err := cmd.Wait(); err != nil {
+		log.Fatal(err)
+	}
+}
