46afab1be0ce0d3d4437def9a03ef84e5b37e443
Pradyumna Kaushik <pkaushi1@binghamton.edu>
2016-11-10 20:05:48 -0500

    Removed these files and integrated them with electron

--- schedulers/proactive_dynamic_capping/src/utilities/utils.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-import "strconv"
-
-// Convert float64 to string
-func FloatToString(input float64) string {
-  // Precision is 2, Base is 64
-  return strconv.FormatFloat(input, 'f', 2, 64)
-}
