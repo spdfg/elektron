311972415483c4d76b2317fc1696374de8d37545
Pradyumna Kaushik <pkaushi1@binghamton.edu>
2017-01-28 21:30:15 -0500

    Added another command-line argument to indicate whether to use the Watts attribute or the ClassToWatts attribute in the workload JSON.

--- scheduler.go
+++ scheduler.go
@@ -21,6 +21,7 @@ var ignoreWatts = flag.Bool("ignoreWatts", false, "Ignore watts in offers")
 var pcplogPrefix = flag.String("logPrefix", "", "Prefix for pcplog")
 var hiThreshold = flag.Float64("hiThreshold", 0.0, "Upperbound for when we should start capping")
 var loThreshold = flag.Float64("loThreshold", 0.0, "Lowerbound for when we should start uncapping")
+var classMapWatts = flag.Bool("classMapWatts", false, "Map Watts to power-class of host in offer")
 
 // Short hand args
 func init() {
@@ -30,6 +31,7 @@ func init() {
 	flag.StringVar(pcplogPrefix, "p", "", "Prefix for pcplog (shorthand)")
 	flag.Float64Var(hiThreshold, "ht", 700.0, "Upperbound for when we should start capping (shorthand)")
 	flag.Float64Var(loThreshold, "lt", 400.0, "Lowerbound for when we should start uncapping (shorthand)")
+	flag.BoolVar(classMapWatts, "cmw", false, "Map watts to power-class of host in offer (shorthand)")
 }
 
 func main() {
