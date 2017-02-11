6ac1b388837e11a96a9ef2928bd77c1986bbde9f
Pradyumna Kaushik <pkaushi1@binghamton.edu>
2017-02-11 01:14:02 -0500

    Added comment to explain the classification of nodes in the cluster into power-classes.

--- constants/constants.go
+++ constants/constants.go
@@ -12,7 +12,11 @@ var Hosts = []string{"stratos-001.cs.binghamton.edu", "stratos-002.cs.binghamton
 	"stratos-005.cs.binghamton.edu", "stratos-006.cs.binghamton.edu",
 	"stratos-007.cs.binghamton.edu", "stratos-008.cs.binghamton.edu"}
 
-// Classification of the nodes in the cluster based on their power consumption.
+/*
+ Classification of the nodes in the cluster based on their Thermal Design Power (TDP).
+ The power classes are labelled in the decreasing order of the corresponding TDP, with class A nodes
+ 	having the highest TDP and class C nodes having the lowest TDP.
+*/
 var PowerClasses = map[string]map[string]bool{
 	"A": map[string]bool{
 		"stratos-005.cs.binghamton.edu": true,
