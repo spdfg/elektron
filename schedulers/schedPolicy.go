1bee742588c23ba63cdbfca74ef0ba9c5643ec78
Pradyumna Kaushik <pkaushi1@binghamton.edu>
2018-09-23 19:19:16 -0400

    fixed import.

--- schedulers/schedPolicy.go
+++ schedulers/schedPolicy.go
@@ -1,7 +1,7 @@
 package schedulers
 
 import (
-	"bitbucket.org/sunybingcloud/electron/def"
+	"bitbucket.org/sunybingcloud/elektron/def"
 	mesos "github.com/mesos/mesos-go/api/v0/mesosproto"
 	sched "github.com/mesos/mesos-go/api/v0/scheduler"
 	"log"
