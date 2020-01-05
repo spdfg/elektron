78f533fe21b2f5d630a48ffe098a6670055fe7d9
Renan DelValle <commit@ridv.xyz>
2020-01-04 17:15:16 -0800

    Adding test for happy path retrieving max power.

--- rapl-daemon/util_test.go
+++ rapl-daemon/util_test.go
@@ -1,14 +1,42 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 // TODO(rdelvalle): Add more thourough testing. Generate mock files
 // that mimic the powercap subsystem and create test to operate on it.
-func TestCap(t *testing.T) {
+func TestCapNode(t *testing.T) {
 
 	err := capNode("/sys/devices/virtual/powercap/intel-rapl", 95)
 
 	if err != nil {
 		t.Fail()
 	}
+}
+
+func TestMaxPower(t *testing.T) {
+	const maxWattage uint64 = 1500000
+
+	tmpfile, err := ioutil.TempFile("", maxPowerFileShortWindow)
+	assert.NoError(t, err)
+
+	defer os.Remove(tmpfile.Name())
+
+	fmt.Println(tmpfile.Name())
+	_, err = tmpfile.Write([]byte(strconv.FormatUint(maxWattage, 10)))
+	assert.NoError(t, err)
+
+	maxWatts, err := maxPower(tmpfile.Name())
+	assert.NoError(t, err)
+	assert.Equal(t, maxWattage, maxWatts)
+
+	err = tmpfile.Close()
+	assert.NoError(t, err)
 }
